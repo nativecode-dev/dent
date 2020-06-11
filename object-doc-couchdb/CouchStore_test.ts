cc09327137c02c610b037d78c9090138bfb0142a
Mike Pham <reginfo@nativecode.com>
2020-06-11 19:30:46 -0400

    chore: ignore CouchStore test

--- object-doc-couchdb/CouchStore_test.ts
+++ object-doc-couchdb/CouchStore_test.ts
@@ -3,17 +3,17 @@ import { config } from './test_deps.ts'
 
 const cfg = config()
 
-Deno.test('should create instance', async () => {
-  const options = {
-    auth: {
-      password: cfg.COUCHDB_PASSWORD || '',
-      username: cfg.COUCHDB_USERNAME || '',
-    },
-    endpoint: cfg.COUCHDB_ENDPOINT || 'localhost',
-    options: {},
-  }
+// Deno.test('should create instance', async () => {
+//   const options = {
+//     auth: {
+//       password: cfg.COUCHDB_PASSWORD || '',
+//       username: cfg.COUCHDB_USERNAME || '',
+//     },
+//     endpoint: cfg.COUCHDB_ENDPOINT || 'localhost',
+//     options: {},
+//   }
 
-  const store = new CouchStore(options)
+//   const store = new CouchStore(options)
 
-  await store.create('dent-tests')
-})
+//   await store.create('dent-tests')
+// })
