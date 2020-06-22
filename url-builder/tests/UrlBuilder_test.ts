372b41d6418fc8215c65a06142db9f815fd69ae0
Mike Pham <reginfo@nativecode.com>
2020-06-22 04:37:19 -0400

    chore: url-builder

--- url-builder/tests/UrlBuilder_test.ts
+++ url-builder/tests/UrlBuilder_test.ts
@@ -7,7 +7,7 @@ Deno.test('should parse url', () => {
   assertEquals(builder.toUrl(), 'http://localhost/')
 })
 
-Deno.test('should parse url with ending slash', () => {
+Deno.test('should parse url with trailing slash', () => {
   const builder = UrlBuilder.parse('http://localhost/')
   assertEquals(builder.toUrl(), 'http://localhost/')
 })
@@ -37,22 +37,27 @@ Deno.test('should parse url with port', () => {
   assertEquals(builder.toUrl(), 'http://localhost:80/test?name=value&name2=value')
 })
 
-Deno.test('should parse with authentication', () => {
+Deno.test('should parse url with authentication', () => {
   const builder = UrlBuilder.parse('http://admin:test@localhost:80/test?name=value&name2=value')
   assertEquals(builder.toUrl(true), 'http://admin:test@localhost:80/test?name=value&name2=value')
 })
 
-Deno.test('should parse with authentication, no output', () => {
+Deno.test('should parse url with authentication, no output', () => {
   const builder = UrlBuilder.parse('http://admin:test@localhost:80/test?name=value&name2=value')
   assertEquals(builder.toUrl(), 'http://localhost:80/test?name=value&name2=value')
 })
 
-Deno.test('should parse with authentication without port, no output', () => {
+Deno.test('should parse url with authentication without port, no output', () => {
   const builder = UrlBuilder.parse('http://admin:test@localhost/test?name=value&name2=value')
   assertEquals(builder.toUrl(), 'http://localhost/test?name=value&name2=value')
 })
 
-Deno.test('should parse with authentication, with trailing slash', () => {
+Deno.test('should parse url with authentication, with trailing slash', () => {
   const builder = UrlBuilder.parse('http://admin:test@localhost:80/test?name=value&name2=value')
   assertEquals(builder.toUrl(true, true), 'http://admin:test@localhost:80/test/?name=value&name2=value')
+})
+
+Deno.test('should parse ftp url with authentication, with trailing slash', () => {
+  const builder = UrlBuilder.parse('ftp://admin:test@localhost:80/test?name=value&name2=value')
+  assertEquals(builder.toUrl(true, true), 'ftp://admin:test@localhost:80/test/?name=value&name2=value')
 })
