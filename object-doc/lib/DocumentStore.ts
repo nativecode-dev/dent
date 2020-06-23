45be11dd45bf0949cbd151e901133127cd17c384
Mike Pham <reginfo@nativecode.com>
2020-06-23 17:15:44 -0400

    chore: add database exists

--- object-doc/lib/DocumentStore.ts
+++ object-doc/lib/DocumentStore.ts
@@ -5,6 +5,7 @@ export interface DocumentStore {
   collection<T extends Document>(name: string, doctype: string): Promise<DocumentCollection<T>>
   create(name: string): Promise<void>
   delete(name: string): Promise<void>
+  exists(name: string): Promise<boolean>
 }
 
 export const DocumentStoreKey: symbol = Symbol('DocumentStore')
