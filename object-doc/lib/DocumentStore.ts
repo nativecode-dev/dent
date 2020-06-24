1e7dcdaf04c2d8563f6ac9d55affc076914e3646
Mike Pham <reginfo@nativecode.com>
2020-06-23 23:34:09 -0400

    chore: update interface

--- object-doc/lib/DocumentStore.ts
+++ object-doc/lib/DocumentStore.ts
@@ -2,7 +2,7 @@ import { Document } from './Document.ts'
 import { DocumentCollection } from './DocumentCollection.ts'
 
 export interface DocumentStore {
-  collection<T extends Document>(name: string, doctype: string): Promise<DocumentCollection<T>>
+  collection<T extends Document>(name: string, doctype: string): DocumentCollection<T>
   create(name: string): Promise<void>
   delete(name: string): Promise<void>
   exists(name: string): Promise<boolean>
