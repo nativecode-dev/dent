ea4dc4b5de06bc6c0b26d43f1031a89e592fe51e
Mike Pham <reginfo@nativecode.com>
2020-06-28 23:47:37 -0400

    chore: update getHostName

--- sysinfo/lib/SystemInfo.ts
+++ sysinfo/lib/SystemInfo.ts
@@ -1,3 +1,9 @@
-export function getHost(): string {
-  return Deno.env.get('HOST') || Deno.hostname() || 'localhost'
+export function getHost(fullhost: boolean = false): string {
+  const hostname = Deno.env.get('HOST') || Deno.hostname() || 'localhost'
+
+  if (fullhost) {
+    return hostname
+  }
+
+  return hostname.split('.').slice(0, 1).join('')
 }
